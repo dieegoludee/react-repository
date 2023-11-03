566beeb2558c1761d40de25b04ebfe64159dd635
Diego Ludeña <diegolude.martin@gmail.com>
2023-11-03 11:57:21 +0100

    Update repo with third project

---
+++ projects/03-mouse-follower/src/components/FollowMouse.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react'
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    console.log('efecto ', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('handleMove', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+    }
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+    /**
+       * cleanUp
+       * -> cuando el componente se desmonta
+       * -> cuando cambian las dependencias, antes de ejecutar el efecto de nuevo
+       */
+    return () => {
+      console.log('cleanUp')
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
+  return (
+    <main>
+      <h3>Proyecto 3</h3>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </main>
+  )
+}
+
+export default FollowMouse
