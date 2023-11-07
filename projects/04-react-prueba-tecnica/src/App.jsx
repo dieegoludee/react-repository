82b95375da28871b0b91d24764f8851b8899d1ee
Diego Ludeña <diegolude.martin@gmail.com>
2023-11-07 12:31:44 +0100

    Refactoring App.jsx

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,40 +1,44 @@
 import { useEffect, useState } from 'react'
+import './App.css'
+import '../style.css'
+import { getRandomFact, getRandomImage } from './services/facts'
 
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`
 // const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
 export function App () {
   const [fact, setFact] = useState()
   const [imageUrl, setImageUrl] = useState()
+  // const [factError, setFactError] = useState()
 
   // No se puede usar React Query, SWR, axios, apollo, etc
+  // Para recuperar la cita al cargar la página
   useEffect(() => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then(response => response.json())
-      .then(data => {
-        const { fact } = data
-        setFact(fact)
-
-        // Recuperar primeras 3 palabras de fact
-        // const firstWord = fact.split(' ').slice(0, 3).join(' ')
-        const threeFirstWord = fact.split(' ', 3).join(' ')
-        fetch(`https://cataas.com/cat/says/${threeFirstWord}?fontSize=50&fontColor=red`)
-          // Recuperamos la 'url' directamente del objeto
-          // .then(response => response.json())
-          .then(response => {
-            const { url } = response
-            setImageUrl(url)
-          })
-      })
+    getRandomFact()
+      .then(newFact => setFact(newFact))
   }, [])
+
+  // Para recuperar la imagen cada vez que tenemos una cita nueva
+  useEffect(() => {
+    getRandomImage(fact)
+      .then(newImage => setImageUrl(newImage))
+  }, [fact])
+
+  const handleClick = async () => {
+    const newFact = await getRandomFact()
+    setFact(newFact)
+  }
 
   return (
     <main>
       <h1>App Fetching de Gatos</h1>
-      {/* renderizado condicional (&& abreviatura de if), si hay 'fact' lo muestra, sino no */}
-      {fact && <p>{fact}</p>}
-      {imageUrl && <img src={imageUrl} alt={`Image extracted using the first three words for ${fact}`} />}
+      <button onClick={handleClick}>Get new fact</button>
+      <section>
+        {/* renderizado condicional (&& abreviatura de if), si hay 'fact' lo muestra, sino no */}
+        {fact && <p>{fact}</p>}
+        {imageUrl && <img src={imageUrl} alt={`Image extracted using the first three words for ${fact}`} />}
+        {/* {factError && <p>{factError}</p>} */}
+      </section>
     </main>
   )
 }
---
+++ projects/04-react-prueba-tecnica/src/services/facts.js
@@ -0,0 +1,18 @@
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+
+export const getRandomFact = async () => {
+  const response = await fetch(CAT_ENDPOINT_RANDOM_FACT)
+  const data = await response.json()
+  const { fact } = data
+  return fact
+}
+
+export const getRandomImage = async (fact) => {
+  if (!fact) return
+  // Recuperar primeras 3 palabras de fact
+  // const firstWord = fact.split(' ').slice(0, 3).join(' ')
+  const threeFirstWord = fact.split(' ', 3).join(' ')
+  const response = await fetch(`https://cataas.com/cat/says/${threeFirstWord}?fontSize=50&fontColor=red`)
+  const { url } = response
+  return url
+}
