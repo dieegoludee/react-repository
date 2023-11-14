33846419d9b617808f68c95286675c83c37ca263
Diego Ludeña <diegolude.martin@gmail.com>
2023-11-14 15:44:19 +0100

    Create MPAs (Multiple Page Applications)

--- projects/07-react-router/README.md
+++ projects/07-react-router/README.md
@@ -1,8 +1,14 @@
-# React + Vite
+# Proyecto React Router desde cero
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- [x] Instalar el linter
+- [x] Crea una forma de hacer MPAs (Multiple Page Applications)
+- [] Crea una forma de hacer SPAs (Single Page Applications)
+- [] Poder navegar entre páginas con el botón de atrás
+- [] Crear componente Link para hacerlo declarativo
+- [] Crear componente Router para hacerlo más declarativo
+- [] Soportar ruta por defecto (404)
+- [] Soportar rutas con parámetros
+- [] Componente <Route /> para hacerlo declarativo
+- [] Lazy Loading de las rutas
+- [] Testing
+- [] Publicar el paquete en NPM
--- projects/07-react-router/src/App.jsx
+++ projects/07-react-router/src/App.jsx
@@ -1,34 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '../../../../../../../../vite.svg'
 import './App.css'
 
-function App () {
-  const [count, setCount] = useState(0)
-
+function HomePage () {
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Home</h1>
+      <p>Esta es la página Home de ejemplo para crear un React Router desde cero</p>
+      <a href='/about'>Ir a sobre nosotros</a>
     </>
+  )
+}
+
+function AboutPage () {
+  return (
+    <>
+      <h1>About</h1>
+      <img
+        src='https://res.cloudinary.com/practicaldev/image/fetch/s--rq2k992d--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/i/uoztlt50jlvdsizrvyz8.png'
+        width={350}
+        height={250}
+      />
+      <p>Esta es la página About de ejemplo para crear un React Router desde cero</p>
+      <a href='/'>Ir a home</a>
+    </>
+  )
+}
+
+function App () {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname)
+  return (
+    <main>
+      <h1>React Router</h1>
+      {currentPath === '/' && <HomePage />}
+      {currentPath === '/about' && <AboutPage />}
+    </main>
   )
 }
 
